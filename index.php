4464b000309dc33077e0e2161e3bd58d9c309158
Anton Babakhin <anton-098@yandex.ru>
2018-03-21 02:15:42 +0300

    'first'

--- index.php
+++ index.php
@@ -2,14 +2,31 @@
 
   require 'vendor/autoload.php';
   
-  $app = new Silex\Application();
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
   
-  $app->get('/hello/{name}', function($name) use ($app) {
-	 return 'Hello '.$app->escape($name); 
+  $app = new Silex\Application(); 
+  
+  
+  if (isset($_GET['public'])) {
+	$app->after(function (Request $request, Response $response) {
+		$response = new Response();
+		$response->headers->set('Access-Control-Allow-Origin', '*');
+		$response->headers->set('Content-type', 'text/plain; charset=utf-8');
+		$response->headers->set('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+	});
+  }
+  
+  $app->get('/print', function(){
+	 return file_get_contents(basename(__FILE__)); 
   });
   
-  $app->get('/date', function(){
-	 return date('r'); 
+  $app->get('/author', function(){
+	 return '<h4 title="GossJS" id="author">Антон Бабахин</h4>'; 
+  });
+  
+  $app->get('/info', function(){
+	 return phpinfo(); 
   });
   
   
